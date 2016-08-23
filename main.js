8f3eddf96a76f44365b3f1a95800f5aa41038ad5
Patrick Gibson <nosbigtap@gmail.com>
2016-08-23 18:42:43 +0000

    lunch time win

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-angular.module("myApp",[])
-    .controller("clickButt",buttonController);
-
-    function buttonController() {
-        var 
-    }
+var app = angular.module('myApp', []);
+    app.controller('myCtrl', function($scope) {
+        $scope.showMe = false;
+        $scope.myFunc = function() {
+            $scope.showMe = !$scope.showMe;
+    };
+});
