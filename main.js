b0edc20b4815ad81729a35b6a5b334c72bba3b3b
Patrick Gibson <nosbigtap@gmail.com>
2016-08-23 19:56:23 +0000

    one button works

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var app = angular.module('myApp', []);
-    app.controller('myCtrl', function($scope) {
-        $scope.showMe = false;
-        $scope.myFunc = function() {
-            $scope.showMe = !$scope.showMe;
+app.controller('myCtrl', function($scope) {
+    $scope.showMe = false;
+    $scope.myFunc = function() {
+        $scope.showMe = !$scope.showMe;
     };
 });
